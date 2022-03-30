8eb4d96f637e7e407fb13d00f58b0eeae802de8d
Jessica Gallo <jgallo2895@gmail.com>
2022-03-30 04:09:10 -0500

    added message and link to github for more issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,25 @@
 var issuesContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var getRepoIssues = function(repo) {
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+    fetch(apiUrl).then(function(response) {
+        //request was successful
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayIssues(data);
+
+                //check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
+        } else {
+            alert("There was a problem with your request!");
+        }
+    });
+};
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -9,8 +30,8 @@ var displayIssues = function(issues) {
         //create a link element to take users to the issue on github
         var issuesEl = document.createElement("a");
         issuesEl.classList = "list-item flex-row justify-space-between align-center";
-        issuesEl.setAttribute = ("href", issues[i].html_url);
-        issuesEl.setAttribute = ("target", "_blank");
+        issuesEl.setAttribute("href", issues[i].html_url);
+        issuesEl.setAttribute("target", "_blank");
 
         //create span to hold issue title
         var titleEl = document.createElement("span");
@@ -33,19 +54,14 @@ var displayIssues = function(issues) {
     }
 };
 
-var getRepoIssues = function(repo) {
-    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
 
-    fetch(apiUrl).then(function(response) {
-        //request was successful
-        if(response.ok) {
-            response.json().then(function(data) {
-                displayIssues(data);
-            });
-        } else {
-            alert("There was a problem with your request!");
-        }
-    });
+    limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
